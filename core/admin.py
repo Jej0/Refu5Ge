9801fcdf5bba899c32265717a173f3fdde42f0dd
Luxus95 <190210526+Luxus95@users.noreply.github.com>
2026-04-25 01:24:01 +0200

    Register multiple models in Django admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Room, Device, DeviceAttribute, CategorieObjet, ObjetConnecte, Animal, Actualite
 
-# Register your models here.
+admin.site.register(Room)
+admin.site.register(Device)
+admin.site.register(DeviceAttribute)
+admin.site.register(CategorieObjet)
+admin.site.register(ObjetConnecte)
+admin.site.register(Animal)
+admin.site.register(Actualite)
