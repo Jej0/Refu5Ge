9fcedc1dcda122a41f69ac7e3359f2fe652fa65b
Luxus95 <190210526+Luxus95@users.noreply.github.com>
2026-04-25 01:25:24 +0200

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'core'
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.accueil, name='home'),             # Accueil 
+    path('recherche/', views.recherche, name='recherche'),  # Recherche publique
 ]
