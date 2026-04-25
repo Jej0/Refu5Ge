d8295e0ac55e2c27d16084c22cf8bc39cff4838b
Nathaanlennon <nathaanbias@gmail.com>
2026-04-25 23:55:25 +0200

    fichier oubliés

--- test1/urls.py
+++ test1/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("test1/create/", views.add_test, name="test1_create"),
-    path("test1/allRooms/", views.AllRooms.as_view(), name="test1_all_rooms"),
+    path("allRooms/", views.AllRooms.as_view(), name="all_rooms"),
     path("rooms/<int:pk>/", views.RoomDetail.as_view(), name="room_detail"), # pour les details d'une room
     path("items/<int:pk>/", views.ItemDetail.as_view(), name="item_detail"),
     path("rooms/<int:pk>/devices/add/", views.room_device_add, name="room_device_add"),
