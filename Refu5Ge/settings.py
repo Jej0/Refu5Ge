3d7ee65f17c2eceec3381ba1b9224a6a2f59adaa
Nathaanlennon <nathaanbias@gmail.com>
2026-04-27 01:20:58 +0200

    merge de tout et réparations des problemes liés au merge

--- Refu5Ge/settings.py
+++ Refu5Ge/settings.py
@@ -37,14 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rapports',
     'gestion.apps.TodoConfig',
     'core.apps.CoreConfig',
 'rapports.apps.RapportsConfig',
 'accounts.apps.AccountsConfig',
-
-    #####
-    'accounts',
 
 ]
 
